9dfc82cc89ea2c9122eb936026c2ffa7f9c663f1
ratonu <ratonu@kali>
2022-05-17 22:09:08 +0300

    environment variable for the pass

--- database_util/database_config.py
+++ database_util/database_config.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "mysql+mysqldb://root:Megatron99a@localhost:3306/vx_underground"
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL,)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
